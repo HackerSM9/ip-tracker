e59efad8f8941564f869e1a54eb6cedf3458ee12
Samarth Magdum (SM9) <101000624+HackerSM9@users.noreply.github.com>
2022-08-18 15:17:34 +0530

    Update track-ip.py

--- .src/track-ip.py
+++ .src/track-ip.py
@@ -1,5 +1,7 @@
 import requests
-t = ''
+
+y = '\033[0;33m'
+t = '\033[1;36m'
 e = '\033[0m'
 ip = input(t+"Enter IP to Trace it >>> ")
 url = "https://api.ipdata.co/v1/{0}/?api-key=be0f755b93290b4c100445d77533d291763a417c75524e95e07819ad".format(ip)
